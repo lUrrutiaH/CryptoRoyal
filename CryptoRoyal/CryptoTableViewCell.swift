a23d56b7222626b07f44088d1b96cdc5dd8de77d
lUrrutiaH <90396675+lUrrutiaH@users.noreply.github.com>
2021-10-23 04:08:56 -0500

    Final product, Workinggg

--- CryptoRoyal/CryptoTableViewCell.swift
+++ CryptoRoyal/CryptoTableViewCell.swift
@@ -7,10 +7,23 @@
 
 import UIKit
 
-struct CryptoTableViewCellViewModel {
+class CryptoTableViewCellViewModel {
     let name : String
     let symbol: String
     let price: String
+    let iconUrl: URL?
+    var iconData: Data?
+    
+    init(name: String,
+         symbol: String,
+         price: String,
+         iconUrl: URL?
+    ) {
+        self.name = name
+        self.symbol = symbol
+        self.price = price
+        self.iconUrl = iconUrl
+    }
 }
 
 class CryptoTableViewCell: UITableViewCell {
@@ -38,6 +51,12 @@ class CryptoTableViewCell: UITableViewCell {
         return label
     }()
     
+    private let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
     //init
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -45,6 +64,7 @@ class CryptoTableViewCell: UITableViewCell {
         contentView.addSubview(nameLabel)
         contentView.addSubview(symbolLabel)
         contentView.addSubview(priceLabel)
+        contentView.addSubview(iconImageView)
     }
     
     required init?(coder: NSCoder) {
@@ -55,19 +75,27 @@ class CryptoTableViewCell: UITableViewCell {
     override func layoutSubviews() {
         super.layoutSubviews()
         
+        let size: CGFloat = contentView.frame.size.height/1.1
+        iconImageView.frame = CGRect(
+            x: 20,
+            y: (contentView.frame.size.height-size)/2,
+            width: size,
+            height: size
+        )
+        
         nameLabel.sizeToFit()
         priceLabel.sizeToFit()
         symbolLabel.sizeToFit()
         
         nameLabel.frame = CGRect (
-            x: 25,
+            x: 30 + size,
             y: 0,
             width: contentView.frame.size.width/2,
             height: contentView.frame.size.height/2
         )
         
         symbolLabel.frame = CGRect (
-            x: 25,
+            x: 30 + size,
             y: contentView.frame.size.height/2,
             width: contentView.frame.size.width/2,
             height: contentView.frame.size.height/2
@@ -81,12 +109,34 @@ class CryptoTableViewCell: UITableViewCell {
         )
     }
     
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        iconImageView.image = nil
+        nameLabel.text = nil
+        priceLabel.text = nil
+        symbolLabel.text = nil
+    }
     //Configure
     
     func configure(with viewModel: CryptoTableViewCellViewModel) {
         nameLabel.text = viewModel.name
         priceLabel.text = viewModel.price
         symbolLabel.text = viewModel.symbol
+
+        if let data = viewModel.iconData {
+            iconImageView.image = UIImage(data: data)
+        }
+        else   if let url = viewModel.iconUrl {
+            let task = URLSession.shared.dataTask(with: url) { [weak self] data, _, _ in
+                if let data = data {
+                    viewModel.iconData = data
+                    DispatchQueue.main.async {
+                        self?.iconImageView.image = UIImage(data: data)
+                    }
+                }
+            }
+            
+            task.resume()
+        }
     }
-    
 }
